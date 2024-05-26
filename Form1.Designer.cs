a54c1a4749b57f7de0398c9a4490d3feb524ab3d
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-05-25 21:04:47 -0500

    Hangman Winform Layout
    Created hangman layout on winform. 
    The groupbox containing the buttons is where the user will be making their guess. The user will be able to submit a letter or a word. 
    The groubox used to the left of the form will display where the hangman is. 
    The groupbox used above the buttons will contain the underscores along with a label to tell the user how long the word is.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(panel1);
+            groupBox1.Location = new Point(539, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(249, 386);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(12, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(513, 257);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox2);
+            groupBox3.Controls.Add(textBox1);
+            groupBox3.Controls.Add(button2);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Location = new Point(12, 275);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(513, 123);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 27);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(243, 356);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(144, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Submit Letter";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(0, 70);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 34);
+            button2.TabIndex = 1;
+            button2.Text = "Submit Word";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(162, 30);
+            textBox1.MaxLength = 1;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(33, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(162, 73);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(341, 229);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Word Length:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(844, 414);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Hangman Game";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Panel panel1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private TextBox textBox2;
+        private Label label1;
+    }
+}
